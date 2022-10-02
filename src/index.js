3a0155a26ca475ec1d6acb3c12f437d321637eeb
Harith Al-Safi <harith.alsafi@gmail.com>
2022-10-02 21:11:42 +0300

    basic setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// renders our app compoments to public/index.html in the root div 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
